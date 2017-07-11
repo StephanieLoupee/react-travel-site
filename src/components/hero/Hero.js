eae8f164594e4200a405c9a90114b5b758ee5888
Eric Schwartz <eric.schwartz@theironyard.com>
2017-07-11 11:21:03 -0400

    react-bootstrap complete

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,34 +3,27 @@ import React, { Component } from 'react';
 import italyImage from './assets/images/italy.jpg'
 import airplaneImage from './assets/images/airplane.jpg'
 
+import { Carousel } from 'react-bootstrap';
+
 class Hero extends Component {
   render() {
     return (
-      <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-
-        <ol className="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        </ol>
-
-        <div className="carousel-inner" role="listbox">
-          <div className="item active">
-            <img src={italyImage} alt="this is an alt tag" />
-          </div>
-          <div className="item">
-            <img src={airplaneImage} alt="stuff" />
-          </div>
-        </div>
-
-        <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
+      <Carousel>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src={italyImage}/>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src={airplaneImage}/>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     );
   }
 }
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
 
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
+
 class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-inverse">
-        <div className="container-fluid">
-           <div className="navbar-header">
-            <a className="navbar-brand" href="/">My Travel Site</a>
-          </div>
-          <ul className="nav navbar-nav navbar-right">
-              <li>
-                  <a href="/home.html">Home</a>
-              </li>
-
-              <li>
-                  <a href="/about.html">About</a>
-              </li>
-
-              <li>
-                  <a href="/stuff.html">Stuff</a>
-              </li>
-
-              <li>
-                  <a href="/contact.html">Contact</a>
-              </li>
-
-          </ul>
-        </div>
-      </nav>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
